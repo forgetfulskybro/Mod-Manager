fb7e96555293b61b5f20adab801f1fc4b4f52f22
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2025-02-17 19:42:06 -0500

    Honestly don't remember what I added to this but I'm sure it's fine :)

--- manager.py
+++ manager.py
@@ -21,7 +21,7 @@ def listMods(p = False):
 
     for x, item in enumerate(json_object):
         if item != "game":
-           if (p): print(x, ".", item)
+           if (p): print(f"{x}. {item}")
 
     return json_object
 
@@ -176,13 +176,9 @@ def uninstallMods():
 
 def write2JsonFile(new_data, filename='mods.json'):
     with open(filename,'r+') as file:
-        # First we load existing data into a dict.
         file_data = json.load(file)
-        # Join new_data with file_data
         file_data[list(new_data.keys())[0]] = new_data.get(list(new_data.keys())[0])
-        # Sets file's current position at offset.
         file.seek(0)
-        # convert back to json.
         json.dump(file_data, file, indent = 4)
 
 def removeAllJsonData(filename):
@@ -195,14 +191,19 @@ def removeAllJsonData(filename):
             json.dump({ }, file2, indent = 4)
 
 def removeFromJsonFile(bye, filename='mods.json'):
-    with open(filename,'r') as file:
-        # First we load existing data into a dict.
-        file_data = json.load(file)
-        # pop bye
-        file_data.pop(bye)
-        with open(filename,'w') as file2:
-            # convert back to json.
-            json.dump(file_data, file2, indent = 4)
+    try:
+        with open(filename,'r') as file:
+            file_data = json.load(file)
+            if bye in file_data:
+                file_data.pop(bye)
+                with open(filename,'w') as file2:
+                    json.dump(file_data, file2, indent = 4)
+            else:
+                print(f"Mod '{bye}' not found in {filename}") 
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.") 
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON in '{filename}'.  Is it valid JSON?")
 
 def startJsonFile():
     if not os.path.exists("mods.json"):
@@ -237,7 +238,7 @@ if __name__ == '__main__':
             mods = getReminder()
             for mod in mods:
                 m = mods[str(mod)]
-                print(f"{m["name"]}: {m["version"]}\nNew Version: {m["updated_version"]}\n")
+                print(f"{m["name"]}: {m["version"]}\nNew Version: {m["updated_version"]}\nLink: https://www.nexusmods.com/cyberpunk2077/mods/{str(mod)}\n")
             print("To turn off this reminder, input \"stop\"\n------------------------------------------")
 
         # DISPLAY MENU
